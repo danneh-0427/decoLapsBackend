5d1025c5968a7c6c7390cc19599f6d5cf46c6dfd
조준현 <107996828+danneh-0427@users.noreply.github.com>
2022-12-06 03:45:38 +0900

    Delete hooks directory

--- hooks/useDecoBalance.ts
+++
@@ -1,9 +0,0 @@
-import { Keplr } from "@keplr-wallet/types";
-import { useEffect, useState } from "react";
-import { chainInfo } from "../configs/chain";
-import { useAddress } from "./useAddress";
-import { useSigningClient } from "./useCosmJSClient";
-
-export function useDecoBalance(keplr: Keplr | null) {
-    const signingClient = useSigningClient(keplr);
-}
