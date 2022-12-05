7026cbfd862492d5c42be2f5db12f861031c7def
조준현 <107996828+danneh-0427@users.noreply.github.com>
2022-12-06 03:46:31 +0900

    Delete hooks directory

--- hooks/useCosmJSClient.ts
+++
@@ -1,5 +0,0 @@
-import { defaultRegistryTypes as defaultStargateTypes, SigningStargateClient, StargateClient } from "@cosmjs/stargate";
-import { Registry } from "@cosmjs/proto-signing";
-import { Keplr } from "@keplr-wallet/types";
-import { chainInfo } from "../configs/chain";
-import { useEffect, useState } from "react";
